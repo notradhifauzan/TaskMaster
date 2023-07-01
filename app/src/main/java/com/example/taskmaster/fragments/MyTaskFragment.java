740efe046967690c12c87f5b9666aa00d6339dd4
najmusyathir <alsyathir@gmail.com>
2023-07-01 17:19:48 +0800

    Rework on bottom_nav_menu.xml

--- app/src/main/java/com/example/taskmaster/fragments/MyTaskFragment.java
+++ app/src/main/java/com/example/taskmaster/fragments/MyTaskFragment.java
@@ -92,7 +92,7 @@ public class MyTaskFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.activity_my_task, container, false);
+        View view = inflater.inflate(R.layout.fragment_my_task, container, false);
 
         context = requireContext(); // get current activity context
 
