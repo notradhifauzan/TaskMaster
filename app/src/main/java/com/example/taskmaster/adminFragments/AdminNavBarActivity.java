3e04317c3d7c347b54ffb069656e5888b0560d15
notradhifauzan <84311250+notradhifauzan@users.noreply.github.com>
2023-07-12 21:24:36 +0800

    implement profile page

--- app/src/main/java/com/example/taskmaster/adminFragments/AdminNavBarActivity.java
+++ app/src/main/java/com/example/taskmaster/adminFragments/AdminNavBarActivity.java
@@ -57,29 +57,6 @@ public class AdminNavBarActivity extends AppCompatActivity {
         loadFragment(new AdminViewAvailableTaskFragment());
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // get the menu inflater
-        MenuInflater inflater = super.getMenuInflater();
-
-        // inflate the menu using our XML menu file id, options_menu
-        inflater.inflate(R.menu.options_menu, menu);
-
-        MenuItem menuItem = menu.findItem(R.id.searchView);
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.logout) {
-            logoutDialogbox();
-            return true;
-        }
-
-        return false;
-    }
-
     private void addTaskDialogBox() {
         // prepare a dialog box with yes and no
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -107,51 +84,10 @@ public class AdminNavBarActivity extends AppCompatActivity {
         alert.show();
     }
 
-    private void logoutDialogbox() {
-        // prepare a dialog box with yes and no
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Logout");
-        builder.setMessage("Log out from the app?");
-
-        // prepare action listener for each button
-        builder.setPositiveButton("Yes",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        doLogout();
-                    }
-                });
-        builder.setNegativeButton("No",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        // do nothing
-                    }
-                });
-
-        // create the alert dialog and show to the user
-        AlertDialog alert = builder.create();
-        alert.show();
-
-    }
-
-    private void doLogout() {
-        // clear the shared preferences
-        SharedPrefManager.getInstance(getApplicationContext()).logout();
-
-        // display message
-        Toast.makeText(getApplicationContext(), "You have successfully logged out", Toast.LENGTH_LONG).show();
-
-        // forward to MainActivity
-        finish();
-        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-    }
-
     private void loadFragment(Fragment fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.frameLayoutContainer, fragment);
         transaction.commit();
     }
-
 
 }
