0308cf51d5834499faf45c52bc6a07caae300027
najmusyathir <alsyathir@gmail.com>
2023-07-13 20:56:16 +0800

    Rework on activity_update_task.xml

--- app/src/main/java/com/example/taskmaster/admins/UpdateTaskActivity.java
+++ app/src/main/java/com/example/taskmaster/admins/UpdateTaskActivity.java
@@ -63,6 +63,7 @@ public class UpdateTaskActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_update_task);
         findViews();
 
